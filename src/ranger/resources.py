a447ab0aa0f566daaf8561022682a0eed4f069bf
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-02-20 14:14:16 +0100

    Fixed the resources.py file

--- src/ranger/resources.py
+++ src/ranger/resources.py
@@ -1,18 +1,17 @@
 import logging; logger = logging.getLogger("ranger.resources")
-
 from threading import Lock
+class Resource:
+    def __init__(self):
+        self.lock = Lock()
 
-LPUPIL = Lock()
-RPUPIL = Lock()
-LLID = Lock()
-RLID = Lock()
-LEYE = CounpoundResource(LPUPIL, LLID)
-REYE = CounpoundResource(RPUPIL, RLID)
-LIDS = CounpoundResource(LLID, RLID)
-EYES = CounpoundResource(LEYE, REYE)
-WHEELS = Lock()
+    def acquire(self, wait = True):
+        self.lock.acquire(wait)
 
-class CounpoundResource:
+    def release(self):
+        self.lock.release()
+
+
+class CompoundResource:
     def __init__(self, *args):
         self.resources = args
 
@@ -29,3 +28,15 @@ class ResourceLockedError(RuntimeError):
         self.value = value
     def __str__(self):
         return repr(self.value)
+
+
+LPUPIL = Resource()
+RPUPIL = Resource()
+LLID = Resource()
+RLID = Resource()
+LEYE = CompoundResource(LPUPIL, LLID)
+REYE = CompoundResource(RPUPIL, RLID)
+LIDS = CompoundResource(LLID, RLID)
+EYES = CompoundResource(LEYE, REYE)
+WHEELS = Resource()
+
