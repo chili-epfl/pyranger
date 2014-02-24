a8df191e73d0c8182d848c5475f44ac8e6ec246e
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-02-24 15:38:54 +0100

    [helpers/plot] Added a x,y,theta plot to display robot traj

--- src/ranger/helpers/plot.py
+++ src/ranger/helpers/plot.py
@@ -1,17 +1,54 @@
-from pylab import *
-import random
-import numpy as np
+from matplotlib import pyplot as plt
+from numpy import arange
+from math import sin, cos
 
 class Plot:
-    def __init__(self, max_y = 10, min_y = 0):
-        ion()
+    def __init__(self, min_y = 0, max_y = 10):
+        plt.ion()
         self.y = [0]
-        self.line, = plot(self.y)
-        ylim([min_y, max_y])
+        self.fig = plt.Figure()
+        self.ax = plt.axes()
+        self.line, = self.ax.plot(self.y)
+        plt.ylim([min_y, max_y])
 
     def add(self, val):
         self.y.append(val)
-        xlim([0, len(self.y) + 1])
+        plt.xlim([0, len(self.y) + 1])
         self.line.set_data(arange(0, len(self.y)), self.y) 
-        draw() 
+        plt.draw() 
 
+class XYPlot:
+    def __init__(self, x = (-5, 5), y = (-5, 5)):
+        plt.ion()
+        self.y = [0]
+        self.x = [0]
+        self.fig = plt.Figure()
+        self.ax = plt.axes()
+        self.line, = self.ax.plot(self.y)
+        plt.ylim(y)
+        plt.xlim(x)
+
+    def add(self, x, y, theta = 0):
+        self.x.append(x)
+        self.y.append(y)
+        self.line.set_data(self.x, self.y)
+
+
+        plt.arrow( x, y, sin(theta) * 0.2, cos(theta) * 0.2, fc="k", ec="k", head_width=0.1, head_length=0.2 )
+        plt.draw()
+        self.ax.artists[0].remove() # remove previous arrow
+
+
+if __name__ == "__main__":
+
+    import time, math
+
+    plot = XYPlot()
+
+    plot.add(1,2,math.pi/6)
+    time.sleep(1)
+    plot.add(2,2, math.pi /3)
+    time.sleep(1)
+    plot.add(-3,0, math.pi)
+    time.sleep(1)
+    plot.add(1,0)
