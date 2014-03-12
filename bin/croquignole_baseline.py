bef16bc9ba90cd059f8cdd58e7234d3681900297
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-03-12 17:51:26 +0100

    [expe] Minor fix in croquignole baseline

--- bin/croquignole_baseline.py
+++ bin/croquignole_baseline.py
@@ -41,12 +41,12 @@ with get_robot(dummy = True) as robot:
     def on_toy_added():
         logger.info("Toy added!")
         robot.playsound(SOUNDS["toy_in"])
-        robot.led_pattern(PATTERNS["toy_in"])
+        robot.lightpattern(PATTERNS["toy_in"])
 
     def on_toy_removed():
         logger.info("Toy removed!")
         robot.playsound(SOUNDS["toy_out"])
-        robot.led_pattern(PATTERNS["toy_out"])
+        robot.lightpattern(PATTERNS["toy_out"])
 
 
 
@@ -54,8 +54,8 @@ with get_robot(dummy = True) as robot:
     logger.info("Waiting for the lolette to be removed...")
     robot.on("lolette", value = True).do(on_lolette)
     robot.on("lolette", value = False).do(on_lolette_removed)
-    #robot.on("scale", increase = 100).do(on_toy_added)
-    #robot.on("scale", decrease = 100).do(on_toy_removed)
+    robot.on("scale", increase = 100).do(on_toy_added)
+    robot.on("scale", decrease = 100).do(on_toy_removed)
 
     while True:
         time.sleep(0.5)
