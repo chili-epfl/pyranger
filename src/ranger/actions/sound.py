62fb10c4c8675e8ab735c8bc1e96262b706b644b
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-03-12 17:56:30 +0100

    [actions/sound] Added a pyaudio interface

--- src/ranger/actions/sound.py
+++ src/ranger/actions/sound.py
@@ -2,9 +2,10 @@ import logging; logger = logging.getLogger("ranger.sound")
 from ranger.decorators import action, lock
 from ranger.resources import *
 
+from playwave import playwave
+
 @action
 @lock(AUDIO)
 def playsound(robot, file):
-    logger.error("playsound not yet implemented!")
-
+    playwave(file)
 
