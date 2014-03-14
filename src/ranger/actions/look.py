230e8337f8bc06f3db3b6cae151f912eaefbc893
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-03-14 11:00:53 +0100

    [actions/look] Added lookat and track actions

--- src/ranger/actions/look.py
+++ src/ranger/actions/look.py
@@ -1,8 +1,45 @@
 import logging; logger = logging.getLogger("ranger.look")
 import time
+import math
 from random import uniform as rand
 from ranger.decorators import action, lock
 from ranger.resources import *
+from ranger.signals import ActionCancelled
+
+def clamp(v, vmin, vmax):
+    return max(vmin, min(vmax, v))
+
+
+@action
+@lock(EYES)
+def lookat(robot, pose):
+    placeeyes(robot, pose)
+
+@action
+@lock(EYES)
+def track(robot, pose):
+    try:
+        while True:
+            placeeyes(robot, pose)
+            time.sleep(0.2)
+    except ActionCancelled:
+        robot.eyes(0,0)
+
+
+def placeeyes(robot, pose):
+    pan, tilt = robot.pose.pantilt(pose, "eyes_link")
+    if pan < -math.pi/2 or pan > math.pi/2:
+        #out of field of view!
+        #TODO: turn the robot? -> maybe yes if the WHEELS are not locked?
+        return
+
+    # vertical field of view
+    tilt = clamp(tilt, -math.pi/4, math.pi/3)
+
+    x = pan/(math.pi/2) * 100
+    y = tilt/(math.pi/2) * 100
+    robot.eyes(x, y)
+
 
 @action
 @lock(EYES)
