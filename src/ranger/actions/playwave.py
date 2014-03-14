78de06cf33b9c8598994b60251adc000d109a0f5
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-03-14 12:28:06 +0100

    [actions/sound] Make sound playing cancellable

--- src/ranger/actions/playwave.py
+++ src/ranger/actions/playwave.py
@@ -3,45 +3,52 @@ import wave
 import sys
 
 
-p = pyaudio.PyAudio()
+class WavePlayer:
+    def __init__(self):
+        self.p = pyaudio.PyAudio()
 
-def playwave(filename):
+    def play(self, filename):
 
-    # length of data to read.
-    chunk = 1024
-    '''
-    ************************************************************************
-        This is the start of the "minimum needed to read a wave"
-    ************************************************************************
-    '''
-    # open the file for reading.
-    try:
-        wf = wave.open(filename, 'rb')
-    except IOError:
-        print("Could not open sound <%s>!" % filename)
-        return
-    # create an audio object
+        self.playing = True
+        # length of data to read.
+        chunk = 1024
+        '''
+        ************************************************************************
+            This is the start of the "minimum needed to read a wave"
+        ************************************************************************
+        '''
+        # open the file for reading.
+        try:
+            wf = wave.open(filename, 'rb')
+        except IOError:
+            print("Could not open sound <%s>!" % filename)
+            return
+        # create an audio object
 
-    # open stream based on the wave object which has been input.
-    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
-                    channels = wf.getnchannels(),
-                    rate = wf.getframerate(),
-                    output = True)
+        # open stream based on the wave object which has been input.
+        stream = self.p.open(format = self.p.get_format_from_width(wf.getsampwidth()),
+                        channels = wf.getnchannels(),
+                        rate = wf.getframerate(),
+                        output = True)
 
-    # read data (based on the chunk size)
-    data = wf.readframes(chunk)
-
-    # play stream (looping from beginning of file to the end)
-    while data != '':
-        # writing to the stream is what *actually* plays the sound.
-        stream.write(data)
+        # read data (based on the chunk size)
         data = wf.readframes(chunk)
 
-    # cleanup stuff.
-    stream.close()    
+        # play stream (looping from beginning of file to the end)
+        while data != '' and self.playing:
+            # writing to the stream is what *actually* plays the sound.
+            stream.write(data)
+            data = wf.readframes(chunk)
 
-def close_audio():
-    p.terminate()
+        # cleanup stuff.
+        stream.close()    
+
+    def stop(self):
+        self.playing = False
+
+    def close(self):
+        self.playing = False
+        self.p.terminate()
 
 
 if __name__ == "__main__":
@@ -52,5 +59,6 @@ if __name__ == "__main__":
             "Usage: %s filename.wav" % sys.argv[0]
         sys.exit(-1)
 
-    playwave(sys.argv[1])
-    close_audio()
+    player = WavePlayer()
+    player.play(sys.argv[1])
+    player.close()
--- src/ranger/actions/sound.py
+++ src/ranger/actions/sound.py
@@ -1,11 +1,17 @@
 import logging; logger = logging.getLogger("ranger.sound")
 from ranger.decorators import action, lock
 from ranger.resources import *
+from ranger.signals import ActionCancelled
 
-from playwave import playwave
+from playwave import WavePlayer
+
+player = WavePlayer()
 
 @action
 @lock(AUDIO)
 def playsound(robot, file):
-    playwave(file)
+    try:
+        player.play(file)
+    except ActionCancelled:
+        player.stop()
 
