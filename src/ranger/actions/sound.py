cd3ca43d3842f836cfb300ee6158dcfe1f3a9363
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-06-25 17:48:35 +0200

    [sound] Rewritten the 'play' to use a ROS publisher to the audio_player ROS node

--- src/ranger/actions/sound.py
+++ src/ranger/actions/sound.py
@@ -3,15 +3,38 @@ from robots.decorators import action, lock
 from robots.signals import ActionCancelled
 from ranger.res import *
 
-from playwave import WavePlayer
+#from playwave import WavePlayer
 
-player = WavePlayer()
+#player = WavePlayer()
+
+#@action
+#@lock(AUDIO)
+#def playsound(robot, file):
+#    try:
+#        player.play(file)
+#    except ActionCancelled:
+#        player.stop()
+
+class AudioPublisher:
+
+    def __init__(self):
+
+        import rospy
+        from std_msgs.msg import String
+        self.pub = rospy.Publisher('play_audio_file', String, queue_size=1)
+
+    def play(self, file):
+        self.pub.publish(file)
+
+audio_publisher = None
 
 @action
 @lock(AUDIO)
 def playsound(robot, file):
-    try:
-        player.play(file)
-    except ActionCancelled:
-        player.stop()
+    global audio_publisher
+
+    if audio_publisher is None:
+        audio_publisher = AudioPublisher()
+
+    audio_publisher.play(file)
 
